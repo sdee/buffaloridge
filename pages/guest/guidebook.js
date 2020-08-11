3ec15b4483f49e07078704c3f2f72237917be6ff
Sutee Dee <sutee.dee@invitae.com>
2020-08-11 08:20:40 -0700

    Adds stub for guest section

---
+++ pages/guest/guidebook.js
@@ -0,0 +1,15 @@
+import Layout from '../../components/Layout';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Guidebook() {
+  return (
+    <Layout>
+      <Row>
+        <Col>
+          <h2>Guidebook</h2>
+        </Col>
+      </Row>
+    </Layout>
+  );
+}
