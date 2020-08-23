2ef11bb1938908f130181d34ed701e357c987dfa
Sutee Dee <thaiyoshi@gmail.com>
2020-08-22 21:23:29 -0700

    Table of content for walkthrough

--- pages/walkthrough.js
+++ pages/walkthrough.js
@@ -5,6 +5,64 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 
+const SectionInfo = [
+  ["Overview", "overview"],
+  ["Dining Area", "diningarea"],
+  ["Living Room", "livingroom"],
+  ["Kitchen", "kitchen"],
+  ["Master Bedroom", "masterbedroom"],
+  ["Master Bathroom", "masterbathroom"],
+  ["Second Bedroom", "secondbedroom"],
+  ["Second Bathroom", "secondbathroom"],
+  ["View", "view"],
+  ["Deck", "deck"],
+];
+
+const Section = ({ section, images }) => {
+  const [title, key] = section;
+  const imageDisplay = images ? (
+    images.map((image) => {
+      return (
+        <>
+          <br />
+          <img src={urlFor(image.image).width(800).url()} />
+        </>
+      );
+    })
+  ) : (
+    <></>
+  );
+
+  return (
+    <div id={key}>
+      <h3>{title}</h3>
+      {imageDisplay}
+    </div>
+  );
+};
+
+const Sections = ({ images }) => {
+  const sections = SectionInfo.map((section) => (
+    <Section section={section} images={images[section[1]]} />
+  ));
+
+  return sections;
+};
+
+const SideNav = () => {
+  const navs = SectionInfo.map((section) => (
+    <>
+      <Nav.Link href={"#" + section[1]}>{section[0]}</Nav.Link>
+    </>
+  ));
+  return (
+    <Nav className="flex-column">
+      <Nav.Link href="#overview">Top</Nav.Link>
+      {navs}
+    </Nav>
+  );
+};
+
 const Walkthrough = (props) => {
   console.log(props);
   const builder = imageUrlBuilder(sanityClient);
@@ -15,17 +73,18 @@ const Walkthrough = (props) => {
 
   const Section = ({ section, images }) => {
     const [title, key] = section;
-    const imageDisplay = images ? images.map((image) => {
-      return (
-              <>
-                <br />
-                <br />
-                <img src={urlFor(image.image).width(800).url()} />
-              </>
-      )
-    }) : <></>;
-
-    
+    const imageDisplay = images ? (
+      images.map((image) => {
+        return (
+          <>
+            <br />
+            <img src={urlFor(image.image).width(800).url()} />
+          </>
+        );
+      })
+    ) : (
+      <></>
+    );
 
     return (
       <div id={key}>
@@ -36,18 +95,7 @@ const Walkthrough = (props) => {
   };
 
   const Sections = ({ images }) => {
-    const sections = [
-      ["Overview", "overview"],
-      ["Living Room", "livingroom"],
-      ["Dining Area", "diningarea"],
-      ["Kitchen", "kitchen"],
-      ["Master Bedroom", "masterbedroom"],
-      ["Master Bathroom", "masterbathroom"],
-      ["Second Bathroom", "secondbathroom"],
-      ["Deck", "deck"],
-      ["Amenities", "amenities"],
-      ["View", "view"],  
-    ].map((section) => (
+    const sections = SectionInfo.map((section) => (
       <Section section={section} images={images[section[1]]} />
     ));
 
@@ -63,11 +111,7 @@ const Walkthrough = (props) => {
       </Row>
       <Row>
         <Col xl={1} class="stickyTop">
-          <Nav>
-            <Nav.Link href="#overview">Top</Nav.Link>
-            <Nav.Link href="#livingroom">Living Room</Nav.Link>
-            <Nav.Link href="#kitchen">Kitchen</Nav.Link>
-          </Nav>
+          <SideNav />
         </Col>
         <Col xl={4}>
           <Sections images={props.imagesBySection} />
@@ -80,7 +124,7 @@ const Walkthrough = (props) => {
 Walkthrough.getInitialProps = async function (context) {
   const { images = "" } = context.query;
   const query =
-    '*[_type == "featureImage" && section in ["overview", "kitchen"]] | order(section asc, order asc)';
+    '*[_type == "featureImage" && section in ["overview",  "diningarea", "livingroom", "kitchen", "masterbedroom", "masterbathroom", "secondbedroom", "secondbathroom", "deck", "view"]] | order(section asc, order asc)';
   const featuredImages = await sanityClient.fetch(query, { images });
   console.log(featuredImages);
   //create map of lists keyed by section
--- sanity/schemas/featureImage.js
+++ sanity/schemas/featureImage.js
@@ -1,37 +1,36 @@
-
 export default {
-    name: 'featureImage',
-    type: 'document',
-    title: 'featureImage',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title',
-        description: 'Internal'
-      },
-      {
-        name: 'caption',
-        type: 'string',
-        title: 'Caption',
-        description: 'Displayed'
-      },
-      {
-        name: 'section',
-        type: 'string',
-        title: 'Section',
-      },
-      {
-        name: 'image',
-        type: 'image',
-        title: 'Image'
-      },
-      { name: 'order', type: 'number', title: 'Order'}
-    ],
-    preview: {
-      select: {
-        title: 'title',
-        media: 'image'
-      }
-    }
-}
+  name: "featureImage",
+  type: "document",
+  title: "featureImage",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+      description: "Internal",
+    },
+    {
+      name: "caption",
+      type: "string",
+      title: "Caption",
+      description: "Displayed",
+    },
+    {
+      name: "section",
+      type: "string",
+      title: "Section",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+    },
+    { name: "order", type: "number", title: "Order" },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      media: "image",
+    },
+  },
+};
