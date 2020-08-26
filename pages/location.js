c7b0d0c0682c405a78d36b6bbea04b97e472e2a5
Sutee Dee <thaiyoshi@gmail.com>
2020-08-25 22:26:56 -0700

    Adds maps to location page

--- pages/location.js
+++ pages/location.js
@@ -7,8 +7,11 @@ export default function Location() {
     <Layout>
       <Row>
         <Col>
-          <h2>Location</h2>
-        </Col>
+          <h2>Our Location</h2>
+          <iframe src="https://www.google.com/maps/d/embed?mid=1-suikabUWawtlqnaPC0ggMGig3EIzj8P" width="640" height="480"></iframe>
+          <h2>Nearby Recommendations</h2>
+          <iframe src="https://www.google.com/maps/d/embed?mid=1apcy4QwL-Xth3XqPh35ryFECqeTO-ajj" width="640" height="480"></iframe>
+      </Col>
       </Row>
     </Layout>
   );
